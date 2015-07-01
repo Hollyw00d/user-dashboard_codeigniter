9d7074fbf487ce520bd3880a2e23040f77946a13
Hollyw00d <matt@mattjennings.net>
2015-07-01 13:05:27 -0700

    main nav skeleton done

--- application/views/userdashboard/home.php
+++ application/views/userdashboard/home.php
@@ -13,7 +13,16 @@
 <body>
 
 <div class="row">
-    <div class="col-md-12"></div>
+    <div class="col-md-12">
+        <nav role="navigation">
+            <ul id="main-nav">
+                <li><a href="<?php echo base_url(); ?>">Home</a></li>
+            </ul>
+            <ul id="utility-nav" class="float-right">
+                <li><a href="#">Sign In</a></li>
+            </ul>
+        </nav>
+    </div>
 
 </div>
 
